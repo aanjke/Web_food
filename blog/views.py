4cdb3d697ab4ca429c2839aaf29452174a6a9c41
my comp <aanjke@gmail.com>
2022-06-16 16:31:35 +0300

    create base.html and make some js

---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+
+from blog.models import Post
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs.get("slug"))
+
+
+
+def home(request):
+    return render(request, 'base.html')
