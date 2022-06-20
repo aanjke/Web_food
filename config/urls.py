348301bed21b132739c1b429a05c6d0b81c1d806
aanjke <anajke@gmail.com>
2022-06-20 22:15:07 +0300

    added home.html

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    path('^ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('blog.urls'))
 ]
 
